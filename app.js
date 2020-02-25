1f5514a7ca0004ab9dfa618e69b478c2b11e2998
will-ing <willkoger@gmail.com>
2020-02-24 18:12:43 -0800

    added aceValid & sumArr function

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 'use strict'
 
+var playerArr = [];
+// builds new player object
 function NewPlayer(name) {
   this.name = name;
-  this.hand = [];
+  this.hand = [1, 14];
   this.score = 0;
   this.bet = 0;
+  playerArr.push(this)
 }
 
 function dealer(){
   this.hand = [];
   this.score = 0;
+}
+
+//validate ace value
+function aceValid(cardDealt){
+  while(cardDealt === 'ace'){
+    if(sumArr(playerArr[0].hand) >= 11){
+      cardDealt = 1;
+    }else{
+      cardDealt = 11;
+    }
+  }return(cardDealt)
+}
+
+function sumArr(arr){ //sums up an array
+  var i = 0;
+  var ttl = 0;
+  while (i < arr.length){
+    ttl = ttl + arr[i];
+    i++;
+  }
+  return(ttl);
 }
