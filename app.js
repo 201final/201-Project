d3a7c964375fe6df5b1948ce91649dfbd0c107dd
Iris Leal <iris_leal_m@hotmail.com>
2020-02-24 19:36:42 -0800

    Adding deck contructor and askUserIfWantsToPlay function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 function NewPlayer(name) {
   this.name = name;
@@ -7,7 +7,21 @@ function NewPlayer(name) {
   this.bet = 0;
 }
 
-function dealer(){
+function Dealer(){
   this.hand = [];
   this.score = 0;
+}
+
+function Deck(){
+  this.gameDeck = [];
+}
+
+/*
+Author: Iris
+This function displays a prompt asking to user if he wants to play.
+Functions returns: boolean.
+*/
+function askUserIfWantsToPlay()
+{
+  return( confirm('Hi there! \n Do you want to play BlackJack?'));
 }
