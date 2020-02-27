47bb0c3dfd6ba26d0e202e54938db7bf2f052578
Alex Pedersen <alex.r.pedersen@outlook.com>
2020-02-27 09:37:37 -0800

    bet system

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ function NewPlayer(name) {
   this.hand = [];
   this.score = 0;
   this.bet = 0;
+  this.bank = 1000;
 }
 function NewDealer(){
   this.name = 'Dealer';
@@ -20,12 +21,51 @@ var player, dealer;
 // MAIN function which xs the game
 startGame();
 
+//Bet system
+function validateBet(){
+  var betValue = document.forms["bettool"]["bet"].value;
+  if(betValue < 0) {
+    alert("Bet must be greater than 0");
+    return false;
+  }
+  if(betValue % 100 != 0) {
+    alert("Bet must be a multiple of 100");
+    return false;
+  }
+  // Once we've passed validation, disable the form and place the bet
+  toggleBetForm(true);
+  placeBet(betValue);
+}
+function toggleBetForm(isDisabled){
+  document.getElementById("Bet").disabled = isDisabled;
+  document.getElementById("placeBetBtn").disabled = isDisabled;
+}
+function placeBet(bet){
+  player.bet = bet;
+}
+function updateBank(isWin, isBlackjack){
+  // Figure out how much was won or lost
+  var betMultiplier = 1;
+  if(isWin){
+    if(isBlackjack){
+      betMultiplier = 1.5;
+    }
+  }
+  else {
+    // They've lost
+    betMultiplier = -1;
+  }
+  // Adjust the bankroll
+  player.bank += player.bet * betMultiplier;
+}
+
 function startGame(){
   if (askUserIfWantsToPlay())
   {
     dealer = new NewDealer();
     player =  new NewPlayer(getName());
     displayMsgInScreen('Have fun ' + player.name + ' !!!');
+    document.getElementById("bank").innerText = player.bank;
     giveInitialCards();
   }
   else
@@ -51,6 +91,9 @@ function resetGame()
   buuton.disabled = false;
   buttonStay.disabled = false;
   displayMsgInScreen(player.name + ' welcome back');
+  // Enable the bet form, show current bank
+  toggleBetForm(false);
+  document.getElementById("bank").innerText = player.bank;
   giveInitialCards();
 }
 
@@ -148,10 +191,32 @@ function pushHand (playerOrDealer){
 }
 
 
-// checks ea hand to see if they have 21
+// checks each hand to see if they have 21
 function check21(playerOrDealer){
   if(playerOrDealer.score > 21){
     // results(`${playerOrDealer.name} has lost`); // playerOrDealer function needs to be created still with a message parameter
+    if(playerOrDealer === dealer){
+      // Dealar busted
+      updateBank(true, false);
+    } if {
+      // Player busted
+      updateBank(false, false);
+    }
+  } if(playerOrDealer.score === 21){
+    results(`${playerOrDealer.name} just hit 21!`);
+    // TODO: Figure out who lost and adjust bank
+    if(playerOrDealer === dealer){
+      // Dealar hit 21
+      updateBank(false, false);
+    } else if (
+      // Player hit 21 (is a blackjack if they only had 2 cards)
+      updateBank(true, player.hand.length === 2)){
+    }
+    askWantsToPlayAgain();
+  } else{
+    return(false); 
+  } 
+}
     displayMsgInScreen(playerOrDealer.name + ' has lost !');
     // endGame();
   } else if(playerOrDealer.score === 21){
@@ -162,11 +227,6 @@ function check21(playerOrDealer){
     return(false);
   }
 }
-
-// function endGame()
-// {
-
-// }
 
 // sums up an arrays
 function sumArr(arr){
@@ -270,3 +330,4 @@ function displayMsgInScreen(messageToDisplay)
   msg = document.getElementById('messages');
   msg.textContent = messageToDisplay;
 }
+
